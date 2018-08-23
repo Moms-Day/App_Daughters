e18b51aad050e13ff3e4c1f8de971d38aaec515c
Jeongdasilver <alal8272@gmail.com>
2018-08-23 17:26:33 +0900

    add CareworkerInformationActivity

---
+++ App_Daughters/app/src/main/java/momsday/app_daughters/CareworkerInformationActivity.java
@@ -0,0 +1,13 @@
+package momsday.app_daughters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CareworkerInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_careworker_information);
+    }
+}
