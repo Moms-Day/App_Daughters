c2ce6fc8c077816755635c005c32fb36a1245e3f
Jeongdasilver <alal8272@gmail.com>
2018-08-24 14:23:26 +0900

    add MyPageActivity

---
+++ App_Daughters/app/src/main/java/momsday/app_daughters/MyPage/MyPageActivity.java
@@ -0,0 +1,15 @@
+package momsday.app_daughters.MyPage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import momsday.app_daughters.R;
+
+public class MyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
