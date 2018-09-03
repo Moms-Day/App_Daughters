152383a63258c1154b304566b5328902f936feea
Jeongdasilver <alal8272@gmail.com>
2018-09-03 19:43:01 +0900

    add CareworkerInformationModel

--- App_Daughters/app/src/main/java/momsday/app_daughters/CareworkerInformation/CareworkerInformationModel.java
+++ App_Daughters/app/src/main/java/momsday/app_daughters/CareworkerInformation/CareworkerInformationModel.java
@@ -1,4 +1,35 @@
 package momsday.app_daughters.CareworkerInformation;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CareworkerInformationModel {
+    @SerializedName("bio")
+    private String introduction;
+
+    @SerializedName("career")
+    private int career;
+
+    @SerializedName("imagePath")
+    private String imagePath;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("oneLineE")
+    private String[] reviews;
+
+    @SerializedName("overall")
+    private int overall;
+
+    @SerializedName("patientInCharge")
+    private int patientNumber;
+
+    @SerializedName("scoreDiligence")
+    private int sincerityScore;
+
+    @SerializedName("scoreKindness")
+    private int kindnessScore;
+
+    @SerializedName("workplace")
+    private String hospitalName;
 }
